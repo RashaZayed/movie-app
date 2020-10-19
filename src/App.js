928c55f86b660f559da096cc375eecacd17ff004
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-18 22:11:44 -0700

    intial code for movie app to search movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Search from './components/Search'
+import axios from 'axios' 
+import Results from './components/Results'
+import Popup from './components/Popup'
+
+
 
 function App() {
+  const [state , setState] = useState({
+    s: '',
+    results:[],
+    selected: {}
+  })
+  const apiurl = 'http://www.omdbapi.com/?apikey=ae66f716';
+
+   
+
+  const handleInput = e => {
+    
+  
+    setState({
+      ...state,
+      s:  e.target.value
+    })
+   
+   
+  }
+
+  const search = e => {
+    if (e.key === 'Enter'){
+      axios(apiurl + '&s=' + state.s).then(({data})=> {
+        console.log(data);
+     
+
+        setState ({
+          ...state ,
+          results : data.Search ,
+        })
+        console.log(state.results);
+      });
+    }
+  }
+
+  
+    
+  const openPopup = id => {
+    console.log(id);
+    axios(apiurl + '&i=' + id).then(({data})=>{
+      let result = data;
+      console.log(data)
+      setState({
+        ...state,
+        selected: result ,
+     
+      })
+    })
+  }
+
+  const closePopup = () => {
+    setState ( {
+     ...state,
+     selected: {},
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <h1>Movie Database</h1>
+        <main>
+          <Search handleInput={handleInput} search={search}/>
+          <Results results={state.results} openPopup = {openPopup} />
+          {(typeof state.selected.Title != 'undefined') ? <Popup selected={state.selected} closePopup={closePopup} />:  false }
+
+        </main>
+       
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Result = props => {
+    const {result , openPopup} = props;
+    
+    return(
+        <div className='result' onClick={() => openPopup(result.imdbID)}>
+            <img src={result.Poster} />
+            <h3>{result.Title}</h3>
+        </div>
+
+    )
+}
+export default Result; 
